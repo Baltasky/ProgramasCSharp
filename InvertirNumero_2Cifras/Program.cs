d8523b36f7c7b3d6678adf80359c615faa15212b
Balta Rdz <baltazaar29@gmail.com>
2020-01-24 18:31:43 -0600

    Programa que invierte números de dos cifras

---
+++ InvertirNumero_2Cifras/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InvertirNumero_2Cifras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, uni, dec, numeroInvertido;
+
+            Console.WriteLine("Ingresa un número de dos cifras: ");
+            num = int.Parse(Console.ReadLine());
+            
+            uni = num % 10;
+            dec = num / 10;
+            numeroInvertido = (uni * 10) + dec;
+
+            Console.WriteLine("El número invertido es: " + numeroInvertido);
+            Console.ReadKey();
+        }
+    }
+}
