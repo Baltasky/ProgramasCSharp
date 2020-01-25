82f78b0ff4694c3294ce1a9b802dc225f8de2025
Balta Rdz <baltazaar29@gmail.com>
2020-01-24 19:28:45 -0600

    Programa que invierte un numero de 3 cifras

---
+++ InvertiNumero_3Cifras/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InvertiNumero_3Cifras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, cen, dec, uni, numeroInvertio;
+
+            Console.WriteLine("Ingresa un número de tres cifras: ");
+            num = int.Parse(Console.ReadLine());
+
+            cen = num / 100;
+            num = num % 100;
+            dec = num / 10;
+            uni = num % 10;
+            numeroInvertio = (uni * 100) + (dec * 10) + cen;
+
+            Console.WriteLine("El número invertido es: " + numeroInvertio);
+            Console.ReadKey();
+        }
+    }
+}
