d7e7544692fc50c18be83d5714ed8b4cc041c354
Balta Rdz <baltazaar29@gmail.com>
2020-01-26 17:21:22 -0600

    Operaciones basicas en C#

---
+++ OperacionesBasicas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OperacionesBasicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, result;
+
+            Console.WriteLine("Ingresa el primer número: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            result = num1 + num2;
+            Console.WriteLine("La suma es: {0}", result);
+            result = num1 - num2;
+            Console.WriteLine("La resta es: {0} - {1} = {2}", num1, num2, result);
+            result = num1 * num2;
+            Console.WriteLine("La multiplicación es: {0}", result);
+            result = num1 / num2;
+            Console.WriteLine("La división es: {0}", result);
+            result = num1 % num2;
+            Console.WriteLine("El residuo es: {0}", result);
+
+            Console.WriteLine("\nPresiona una tecla");
+            Console.ReadKey();
+        }
+    }
+}
