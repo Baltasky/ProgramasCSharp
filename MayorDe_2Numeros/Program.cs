2833c9a859ef306177accf9c2f99daf7cc9dfd42
Balta Rdz <baltazaar29@gmail.com>
2020-02-07 12:33:42 -0600

    Mayor de dos números.

---
+++ MayorDe_2Numeros/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MayorDe_2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Digite el primer número: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite el segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("{0} es mayor que {1} ",num1 ,num2);
+            }
+            else
+            {
+                if(num1 == num2)
+                {
+                    Console.WriteLine("{0} es igual que {1} ",num1 ,num2);
+                }
+                else 
+                {
+                    Console.WriteLine("{0} es menor que {1} ",num1 ,num2);  
+                }
+            }
+            
+
+            //otra forma de resolverlo
+
+            string result;
+
+            if(num1 > num2)
+            {
+                result = "Mayor";
+            }else if (num1 == num2)
+            {
+                result = "Igula";
+            }
+            else
+            {
+                result = "Menor";
+            }
+
+               Console.WriteLine("{0} es {1} que {2} ",num1 ,result ,num2); 
+
+            Console.ReadKey();
+        }
+    }
+}
